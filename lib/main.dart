76e901d234148d14a03a4c93072967d2df752fbc
nikitalazarenko <nikitalazarenko@gmail.com>
2024-03-07 17:18:24 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,57 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(CalculatorApp());
+  runApp(const CalculatorApp());
 }
 
 
 class CalculatorApp extends StatefulWidget {
+  const CalculatorApp({super.key});
+
 
   @override
   _CalculatorAppState createState() => _CalculatorAppState();
 }
 
 class _CalculatorAppState extends State<CalculatorApp> {
+
+  late double num1,num2,ans=0;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Center(child: Text('Калькулятор')),),
+        body: Column(
+          children: [
+            const Text('Введите первое число', style: TextStyle(fontSize: 30),),
+            TextField(onChanged: (value) => num1=double.parse(value),),
+            const Text('Введите второе число', style: TextStyle(fontSize: 30),),
+            TextField(onChanged: (value) => num2=double.parse(value),),
+            Row(mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(child: const Text('+'),onPressed: (){
+                  setState(() {
+                    ans=num1+num2;
+                  });},),
+                  ElevatedButton(child: const Text('-'),onPressed: (){
+                    setState(() {
+                      ans=num1-num2;
+                    });},),
+                  ElevatedButton(child: const Text('*'),onPressed: (){
+                    setState(() {
+                      ans=num1*num2;
+                    });},),
+                  ElevatedButton(child: const Text('/'),onPressed: (){
+                    setState(() {
+                      ans=num1/num2;
+                    });},),
+                  ],
+                  ),
+                  Text(ans.toString(), style: const TextStyle(fontSize: 30),)
+          ],
+        ),
+      ),
+    );
   }
 }
